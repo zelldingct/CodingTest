4ffd7bfc942a4ddbd668494cf3f6f30756570119
zelldingct <zell9931@gmail.com>
2022-07-11 23:59:29 +0900

    220711

---
+++ src/TwoPonters_SlidingWindow/combineTwoArray_1.java
@@ -0,0 +1,48 @@
+package TwoPonters_SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class combineTwoArray_1 {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1++]);
+
+            } else {
+                answer.add(b[p2++]);
+
+            }
+            while (p1 < n) {
+                answer.add(a[p1++]);
+            }
+            while (p2 < m) {
+                answer.add(b[p2++]);
+            }
+
+
+
+        }
+        return answer;
+    }
+
+        public static void main (String[]args){
+            combineTwoArray_1 combineTwoArray = new combineTwoArray_1();
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+            int m = scanner.nextInt();
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = scanner.nextInt();
+            }
+            for (int x : combineTwoArray.solution(n, m, a, b)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
