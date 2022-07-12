7162f4e7cd9de7fb486af3a5c712e24b331dc93e
zelldingct <zell9931@gmail.com>
2022-07-12 15:51:23 +0900

    220712DoneWithQ4

--- src/TwoPonters_SlidingWindow/getCommonElement_2.java
+++ src/TwoPonters_SlidingWindow/getCommonElement_2.java
@@ -1,6 +1,44 @@
 package TwoPonters_SlidingWindow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class getCommonElement_2 {
 
+    public ArrayList<Integer> solution ( int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1=0, p2=0;
+        while(p1<n && p2<m){
+            if(a[p1]==b[p2]){
+                answer.add(a[p1++]);
+                p2++;
+            }else if(a[p1]<b[p2]){
+                p1++;
+            }else{
+                p2++;
+            }
+        }
+        return answer;
+    }
 
+    public static void main(String[] args) {
+        getCommonElement_2 getCommonElement = new getCommonElement_2();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++){
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for(int i=0; i<m; i++){
+            b[i] = scanner.nextInt();
+        }
+        for(int x: getCommonElement.solution(n,m,a,b)){
+            System.out.print(x + " ");
+        }
+    }
 }
---
+++ src/TwoPonters_SlidingWindow/getSerialSequence_4.java
@@ -0,0 +1,34 @@
+package TwoPonters_SlidingWindow;
+
+import java.util.Scanner;
+
+public class getSerialSequence_4 {
+    public int solution (int n, int m, int[] arr){
+        int answer=0;
+        int sum=0;
+        int lt=0;
+
+        for(int rt=0; rt<n; rt++){
+            sum+=arr[rt];
+            if(sum==m){answer++;}
+            while(sum>=m){
+                sum-=arr[lt++];
+                if(sum==m){answer++;}
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        getSerialSequence_4 getSerialSequence = new getSerialSequence_4();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int [] arr = new int[8];
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(getSerialSequence.solution(n,m,arr));
+    }
+}
