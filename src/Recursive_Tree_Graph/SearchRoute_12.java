7c146f1b4e8a29bd5e68054329fb7c02c5366502
zelldingct <zell9931@gmail.com>
2022-07-27 22:12:57 +0900

    220727DoneWithRecursive_Tree_Graph

---
+++ src/Recursive_Tree_Graph/SearchRoute_12.java
@@ -0,0 +1,39 @@
+package Recursive_Tree_Graph;
+
+import java.util.Scanner;
+
+public class SearchRoute_12 {
+    static int n, m, answer=0;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v){
+        if(v==n) answer++;
+        else{
+            for(int i=1; i<=n; i++){
+                if(graph[v][i]==1 && ch[i]==0){
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SearchRoute_12 searchRoute = new SearchRoute_12();
+        Scanner scanner = new Scanner(System.in);
+        n=scanner.nextInt();
+        m=scanner.nextInt();
+        graph=new int[n+1][n+1];
+        ch= new int[n+1];
+        for(int i=0; i<m; i++){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            graph[a][b]=1;
+        }
+        ch[1]=1;
+        searchRoute.DFS(1);
+        System.out.println(answer);
+    }
+}
---
+++ src/Recursive_Tree_Graph/ShortestWayGraph_14.java
@@ -0,0 +1,56 @@
+package Recursive_Tree_Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ShortestWayGraph_14 {
+    //BFS를 이용한 풀이
+    static int n, m;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch, dis;
+
+    public void BFS(int v){
+        Queue<Integer> queue = new LinkedList<>();
+        ch[v]=1;
+        dis[v]=0;
+        queue.offer(v);
+        while(!queue.isEmpty()){
+            int cv=queue.poll();
+            for(int nv:graph.get(cv)){
+                if(ch[nv]==0){
+                    ch[nv]=1;
+                    queue.offer(nv);
+                    dis[nv]=dis[cv]+1;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ShortestWayGraph_14 searchRoute = new ShortestWayGraph_14();
+        Scanner scanner = new Scanner(System.in);
+        n=scanner.nextInt();
+        m=scanner.nextInt();
+        graph=new ArrayList<ArrayList<Integer>>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        ch=new int[n+1];
+        dis=new int[n+1];
+        for(int i=0; i<m; i++){
+            int a=scanner.nextInt();
+            int b=scanner.nextInt();
+            graph.get(a).add(b);
+        }
+        //ch[1]=1;
+        searchRoute.BFS(1);
+        for(int i=2; i<=n; i++){
+        System.out.println(i+" : "+dis[i]);
+        }
+    }
+
+
+}
