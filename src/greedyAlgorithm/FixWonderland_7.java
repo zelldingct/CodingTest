e1cab6f65525796949be937c58c6a4e5ed1511fe
zelldingct <zell9931@gmail.com>
2022-08-05 17:20:32 +0900

    220805DoneWithNum7

--- src/greedyAlgorithm/FixWonderland_7.java
+++ src/greedyAlgorithm/FixWonderland_7.java
@@ -1,6 +1,7 @@
 package greedyAlgorithm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class Info implements Comparable<Info> {
@@ -18,8 +19,17 @@ class Info implements Comparable<Info> {
 }
 
 public class FixWonderland_7 {
+    static int[] unf;
 
-    public static union(){
+    public static int Find(int v){
+        if(v==unf[v]) return v;
+        else return unf[v]=Find(unf[v]);
+    }
+    public static void Union(int a, int b){
+        int fa=Find(a);
+        int fb=Find(b);
+        if(fa!=fb) unf[fa]=fb;
+
 
     }
 
@@ -28,14 +38,38 @@ public class FixWonderland_7 {
         Scanner scanner = new Scanner(System.in);
         int v = scanner.nextInt();
         int e = scanner.nextInt();
+        unf = new int[v + 1];
         ArrayList<Info> arr = new ArrayList<>();
 
-        for(int i=0; i<e; i++) {
+        for (int i = 1; i <= v; i++) {
+            unf[i] = i;
+        }
+
+        for (int i = 0; i < e; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             int c = scanner.nextInt();
-            arr.add(new Info(a,b,c));
+            arr.add(new Info(a, b, c));
         }
-        System.out.println();
+
+        int answer = 0;
+
+        Collections.sort(arr);
+
+        for (Info ob : arr) {
+            int fv1 = Find(ob.s);
+            int fv2 = Find(ob.e);
+            if (fv1 != fv2) {
+                answer += ob.cost;
+                Union(ob.s, ob.e);
+            }
+        }
+        System.out.println(answer);
+        for (int i = 0; i < e; i++) {
+            System.out.println(arr.get(i).s + " " + arr.get(i).e + " " + arr.get(i).cost);
+        }
+        for(int x : unf){
+            System.out.print(x+" ");
+        }
     }
 }
